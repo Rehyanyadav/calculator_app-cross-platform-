600009a15f0193b45bf9418742339844ea079780
root:$rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2024-06-03 21:32:00 +0530

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,217 @@
+import 'package:calculator/pages/assets/buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var userInput = '';
+  var answer = '';
+
+  // Array of button
+  final List<String> buttons = [
+    'C',
+    '+/-',
+    '%',
+    'DEL',
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    'x',
+    '1',
+    '2',
+    '3',
+    '-',
+    '0',
+    '.',
+    '=',
+    '+',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+
+      appBar: AppBar(
+
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        elevation: 40,
+        shadowColor: Colors.blue,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              'Calculator',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.blue),
+            ),
+            Text(
+              '-by alpha Code',
+              style: TextStyle(color: Colors.blue),
+            )
+          ],
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      body: Column(
+
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    userInput,
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    answer,
+                    style: TextStyle(
+                        fontSize: 27,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+              itemCount: buttons.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4),
+              itemBuilder: (BuildContext context, int index) {
+                // Clear Button
+                if (index == 0) {
+                  return MyButton(
+                    buttontapped: () {
+                      setState(() {
+                        userInput = '';
+                        answer = '0';
+                      });
+                    },
+                    buttonText: buttons[index],
+                    color: Colors.blue[50],
+                    textColor: Colors.black,
+                  );
+                }
+                // +/- button
+                else if (index == 1) {
+                  return MyButton(
+                    buttontapped: () {
+                      setState(() {
+                        userInput = userInput.startsWith('-')
+                            ? userInput.substring(1)
+                            : '-' + userInput;
+                      });
+                    },
+                    buttonText: buttons[index],
+                    color: Colors.blue[50],
+                    textColor: Colors.black,
+                  );
+                }
+                // % Button
+                else if (index == 2) {
+                  return MyButton(
+                    buttontapped: () {
+                      setState(() {
+                        userInput += buttons[index];
+                      });
+                    },
+                    buttonText: buttons[index],
+                    color: Colors.blue[50],
+                    textColor: Colors.black,
+                  );
+                }
+                // Delete Button
+                else if (index == 3) {
+                  return MyButton(
+                    buttontapped: () {
+                      setState(() {
+                        userInput =
+                            userInput.substring(0, userInput.length - 1);
+                      });
+                    },
+                    buttonText: buttons[index],
+                    color: Colors.blue[50],
+                    textColor: Colors.black,
+                  );
+                }
+                // Equal_to Button
+                else if (index == 18) {
+                  return MyButton(
+                    buttontapped: () {
+                      setState(() {
+                        equalPressed();
+                      });
+                    },
+                    buttonText: buttons[index],
+                    color: Colors.orange[700],
+                    textColor: Colors.white,
+                  );
+                } else {
+                  return MyButton(
+                    buttontapped: () {
+                      setState(() {
+                        userInput += buttons[index];
+                      });
+                    },
+                    buttonText: buttons[index],
+                    color: isOperator(buttons[index])
+                        ? Colors.blueAccent
+                        : Colors.white,
+                    textColor: isOperator(buttons[index])
+                        ? Colors.white
+                        : Colors.black,
+                  );
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  bool isOperator(String x) {
+    if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
+  }
+
+  void equalPressed() {
+    String finaluserinput = userInput;
+
+    finaluserinput = finaluserinput.replaceAll('x', '*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finaluserinput);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    setState(() {
+      answer = eval.toString();
+    });
+  }
+}
